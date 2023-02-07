3440ee989605618fb0395ed8e1b5c58349a20467
Ronak Rathod <ronakfortune19@gmail.com>
2023-02-07 14:17:09 +0530

    Ronak

---
+++ RonakMweb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RonakMweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
